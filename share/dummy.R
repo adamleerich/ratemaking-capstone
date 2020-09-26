fc303127a6d919f83f1f7f5626337f35ad8425d3
Adam Rich <adamleerich@gmail.com>
2020-09-26 09:41:56 -0400

    Automated catch up commit

---
+++ share/dummy.R
@@ -0,0 +1,21 @@
+
+#' This is a report.
+#' These are comments.
+#' But, because I prefaced
+#' them with "#'"
+#' my comments will look like
+#' report text.
+
+x <- runif(1e3, 0, 10)
+
+# However, this will look like 
+# code comments.
+str(x)
+head(x)
+summary(x)
+hist(x)
+
+x <- c('CA', 'WA', 'OR')
+
+
+
